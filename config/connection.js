462cc0e7e8ea13b3db7c708bc39c365bd88a539b
Drew Jordan <ajordan12@luc.edu>
2023-07-13 17:40:51 -0500

    updates

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect('mongodb://127.0.0.1:27017/social-network-db', {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log(`MongoDB connected: ${conn.connection.host}`);
-//   } catch (err) {
-//     console.error(err);
-//     process.exit(1);
-//   }
-// };
 mongoose.connect('mongodb://127.0.0.1:27017/social-network-db');
 module.exports = mongoose.connection;
-// module.exports = connectDB;
+
+
