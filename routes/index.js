a7158b928f50f9e9c7f925de9b9b3b24f73b1e92
Drew Jordan <ajordan12@luc.edu>
2023-07-12 19:26:21 -0500

    index.js for routes

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const userRoutes = require('./usersRoutes');
+const thoughtRoutes = require('./thoughtsRoutes');
+const reactionRoutes = require('./reactionsRoutes');
+const friendRoutes = require('./friendsRoutes');
+
+module.exports = {
+  userRoutes,
+  thoughtRoutes,
+  reactionRoutes,
+  friendRoutes,
+};
