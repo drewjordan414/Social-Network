b8ff4641f51de280a41ec1b8460187a605af6c3b
Drew Jordan <ajordan12@luc.edu>
2023-07-13 17:30:55 -0500

    updates

--- server.js
+++ server.js
@@ -38,18 +38,18 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Connect to MongoDB
-connectDB(); // Call the db function to establish the connection
+// connectDB(); // Call the db function to establish the connection
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-app.use((req, res) => {
-  res.status(404).send('<h1>404 Error</h1>');
-});
+// app.use((req, res) => {
+//   res.status(404).send('<h1>404 Error</h1>');
+// });
 
-mongoose.connection.once('open', () => {
+connectDB.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
