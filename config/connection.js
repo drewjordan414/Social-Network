525841fa0a2b2b4aaea7f41cfc1475ee967c9c66
Drew Jordan <ajordan12@luc.edu>
2023-07-12 22:56:07 -0500

    updates

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,35 @@
+// const mongoose = require('mongoose');
+// const db = async () => {
+//     try {
+//         const conn = await mongoose.connect(`mongodb://127.0.0.1:27017/social-network-db`, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//             useFindAndModify: false,
+//             useCreateIndex: true,
+//         });
+//         console.log(`MongoDB connected: ${conn.connection.host}`);
+//     } catch (err) {
+//         console.error(err);
+//         process.exit(1);
+//     }
+// };
+// module.exports = db;
+
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(`mongodb://127.0.0.1:27017/social-network-db`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true,
-        });
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/social-network-db', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
+
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,18 +1,44 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const db = require('./config/connection');
+// const routes = require('./routes');
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// // Connect to MongoDB
+// mongoose.connect(db, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+// });
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(routes);
+
+// app.use((req, res) => {
+//   res.status(404).send('<h1>404 Error</h1>');
+// });
+
+// mongoose.connection.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server running on port ${PORT}!`);
+//   });
+// });
+
 const express = require('express');
 const mongoose = require('mongoose');
-const db = require('./config/connection');
+const connectDB = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Connect to MongoDB
-mongoose.connect(db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
+connectDB(); // Call the db function to establish the connection
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -28,3 +54,4 @@ mongoose.connection.once('open', () => {
     console.log(`API server running on port ${PORT}!`);
   });
 });
+
