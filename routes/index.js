399706189c61d32e17368abb6d96840b1e1d24f6
Drew Jordan <ajordan12@luc.edu>
2023-07-12 22:51:12 -0500

    routes index update

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 const userRoutes = require('./apiRoutes/userRoutes');
 const thoughtRoutes = require('./apiRoutes/thoughtsRoutes');
-const reactionRoutes = require('./apiRoutes/reactionsRoutes');
-const friendRoutes = require('./apiRoutes/friendsRoutes');
 
 module.exports = {
   userRoutes,
   thoughtRoutes,
-  reactionRoutes,
-  friendRoutes,
 };
