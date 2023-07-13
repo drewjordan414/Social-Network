50413b97db0cdd3df657acb689c4eedf8d6cda94
Drew Jordan <ajordan12@luc.edu>
2023-07-12 22:33:49 -0500

    thought model update

--- models/Date.js
+++
@@ -1,6 +0,0 @@
-const dateFormat = (date) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return new Date(date).toLocaleDateString(undefined, options);
-};
-
-module.exports = dateFormat;
